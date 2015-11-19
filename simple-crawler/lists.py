a50039d4f300105cff6663de4cb3af1801350fbc
hosein <hs.ramezanpoor@gmail.com>
2015-11-19 15:33:12 +0330

    Code blocks moved to new file

---
+++ simple-crawler/lists.py
@@ -0,0 +1,16 @@
+sites = [
+	# Write your prefered site(s) here
+	'digiato.com',
+	'Cnet.com',
+	'stackoverflow.com',
+]
+
+
+# Write youe Keyword(s) in target.txt file
+# Each keyword in seperate line
+target = open('targets.txt', 'r')
+target = target.read()
+target = target.split('\n')
+# word = [word.append(target.readline) for line in len(target)]
+
+
